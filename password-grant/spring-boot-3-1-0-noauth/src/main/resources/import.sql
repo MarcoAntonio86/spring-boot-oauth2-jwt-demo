75003225f6e3c17681d0f8af8e49f1ce96a70231
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2025-10-23 22:09:43 -0300

    teste varidação BCrypt

--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$5m7LVDZmt6FMZGF8JDEAYeGPDx5ZgkD7e3RfJ0vraaS1PGhl0ng1.');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$5m7LVDZmt6FMZGF8JDEAYeGPDx5ZgkD7e3RfJ0vraaS1PGhl0ng1.');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');

43f8d510a5d055f7f9538cd6a5366deb5c8788c1
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2025-10-23 22:19:44 -0300

    BCrypt password

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,29 +1,17 @@
 package com.devsuperior.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication  {
 	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
-	@Override
-	public void run(String...args) throws Exception{
-		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
-		
-		boolean result = passwordEncoder.matches("123456", "$2a$10$5m7LVDZmt6FMZGF8JDEAYeGPDx5ZgkD7e3RfJ0vraaS1PGhl0ng1.");
-		
-		System.out.println("RESULTADO = " + result);
-	}
+	
 
 	
 }
